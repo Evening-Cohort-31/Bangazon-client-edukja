ce06222d7c553acf4d7eadea34adf2f568fa99f5
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-05-15 18:41:03 -0500

    Loging out will now route to /login

--- components/navbar.js
+++ components/navbar.js
@@ -42,14 +42,17 @@ export default function Navbar() {
               <Link href="/stores/new" className="navbar-item">Interested in selling?</Link>
           }
           <hr className="navbar-divider"></hr>
-          <a className="navbar-item" onClick={
+          <Link className="navbar-item" 
+          href={"/login"}
+          onClick={
             () => {
               localStorage.removeItem('token')
               setIsLoggedIn(false)
+              
             }}
           >
             Log out
-          </a>
+          </Link>
         </div>
       </div>
     )
