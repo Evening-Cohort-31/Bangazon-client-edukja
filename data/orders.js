353143d3c118394bd5e6ba56c605ea0bf7fc6ca1
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-05-26 22:13:43 -0500

    Added custom error handling to completeCurrentOrder

--- data/orders.js
+++ data/orders.js
@@ -16,8 +16,8 @@ export function getOrders() {
   })
 }
 
-export function completeCurrentOrder({orderId, paymentTypeId}) {
-  return fetchWithResponse(`orders/${orderId}`, {
+export async function completeCurrentOrder({orderId, paymentTypeId}) {
+  const res = await fetchWithResponse(`orders/${orderId}`, {
     method: 'PUT',
     headers: {
       Authorization: `Token ${localStorage.getItem('token')}`,
@@ -25,4 +25,6 @@ export function completeCurrentOrder({orderId, paymentTypeId}) {
     },
     body: JSON.stringify({ payment_type: paymentTypeId })
   })
+  if (res?.message) throw new Error(res.message)
+  return res
 }
