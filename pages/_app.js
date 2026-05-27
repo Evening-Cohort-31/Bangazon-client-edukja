6d0df28c780d4a99125e226219890ccd98993911
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-05-26 20:37:27 -0500

    Installed tanstack dev tools and wrapped app with provider and hydration boundary

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
 import '../global.css'
+import {
+  HydrationBoundary, QueryClient, QueryClientProvider
+} from "@tanstack/react-query"
+import { ReactQueryDevtools} from "@tanstack/react-query-devtools"
+import {useState} from "react"
+
 
 export default function Bangazon({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
+  const [queryClient] = useState(() => new QueryClient())
 
   return getLayout(
-    
-      <Component {...pageProps} />
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </HydrationBoundary>
+    </QueryClientProvider>
   )
 }
